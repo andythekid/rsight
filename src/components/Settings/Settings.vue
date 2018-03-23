0c81bcee7e19fd231e141f5d79e4cafbb7eb05a4
Andrey Volklv <evil.bobby@gmail.com>
2018-03-23 16:03:28 +1000

    Settings menu, wallet-app

--- src/components/Settings/Settings.vue
+++ src/components/Settings/Settings.vue
@@ -1,3 +1,50 @@
 <template>
-  <h1>Settings</h1>
+  <div>
+    <div class="row">
+      <div class="submenu col-sm-2">
+        <input class="form-control" placeholder="Search..">
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Coins
+            <span class="badge badge-primary">2</span>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Wallets
+            <span class="badge badge-primary">4</span>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">Rigs
+            <span class="badge badge-primary">3</span>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-10">
+        <form class="form-inline submenu">
+          <div class="form-group mx-sm-3 mb-2">
+            <select class="custom-select">
+              <option selected>Wallet</option>
+              <option value="1">Coin</option>
+              <option value="2">Rig</option>
+            </select>
+          </div>
+          <button type="button" class="btn btn-primary mb-2">Add new</button>
+        </form>
+        <app-wallet></app-wallet>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  import Wallet from './Wallet.vue'
+  export default {
+    components: {
+      appWallet: Wallet
+    }
+  }
+
+</script>
+
+<style>
+  .submenu {
+    padding: 10px;
+  }
+
+</style>
---
+++ src/components/Settings/Wallet.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="col-md-10">
+    <form class="form-horizontal" action="">
+      <div class="row">
+        <div class="col-md-10">
+          <div class="form-group">
+            <div class="row">
+              <label class="col-md-3 control-label col-form-label-sm" for="coin-name">
+                <b>
+                  Test
+                </b>
+              </label>
+              <label class="col-md-2 offset-md-2 control-label col-form-label-sm" for="coin-alias">Alias</label>
+              <div class="col-md-5">
+                <input type="text" class="form-control form-control-sm" id="coin-alias" placeholder="Alias" value="Sluts" disabled>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <label class="col-md-2 control-label col-form-label-sm" for="coin-address">Address</label>
+              <div class="col-md-10">
+                <input type="text" class="form-control form-control-sm" id="coin-address" placeholder="Address" value="123"
+                  disabled>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="form-check col-md-4">
+                <label class="form-check-label col-form-label-sm">
+                  <input class="form-check-input form-control-sm" type="checkbox" name="localWallet"> Local wallet
+                </label>
+              </div>
+              <label class="col-md-2 offset-md-1 control-label col-form-label-sm" for="coin-balance">Balance</label>
+              <div class="col-md-5">
+                <input type="number" class="form-control form-control-sm" id="coin-balance" placeholder="Ballance" value="<%= wallet.ballance / wallet.divider %>"
+                  disabled>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="form-check col-md-7">
+                <label class="form-check-label col-form-label-sm">
+                  <input class="form-check-input form-control-sm" type="radio" name="group" value="nope" checked> No group
+                </label>
+                <label class="form-check-label col-form-label-sm">
+                  <input class="form-check-input form-control-sm" type="radio" name="group" value="same"> Group with same coin
+                </label>
+                <label class="form-check-label col-form-label-sm">
+                  <input class="form-check-input form-control-sm" type="radio" name="group" value="another"> Add to another group
+                </label>
+              </div>
+              <div class="col-md-5">
+                <input class="form-control form-control-sm" type="text" name="group-name" disabled>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="form-check">
+            <label class="form-check-label col-form-label-sm">
+              <input class="form-check-input form-control-sm" type="checkbox" name="power" checked> Active
+            </label>
+          </div>
+          <button class="btn btn-warning btn-block btn-sm">Edit</button>
+          <button class="btn btn-danger btn-block btn-sm">Delete</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
