f31a2e6cffbf22eb7fc25fd2b8cf4d6fb5805b93
Andrey Volklv <evil.bobby@gmail.com>
2018-03-27 19:11:26 +1000

    wallet-modal redesign, vars

--- src/components/Settings/WalletModal.vue
+++ src/components/Settings/WalletModal.vue
@@ -6,55 +6,110 @@
         <div class="col-md-10">
           <div class="form-group">
             <div class="row">
-              <label class="col-md-3 control-label col-form-label-sm" for="coin-name">
-                <b>
-                  1
-                </b>
-              </label>
-              <label class="col-md-2 offset-md-2 control-label col-form-label-sm" for="coin-alias">Alias</label>
-              <div class="col-md-5">
-                <input type="text" class="form-control form-control-sm" id="coin-alias" placeholder="Alias" disabled>
-              </div>
+              <select class="form-control form-control-sm custom-select col-md-5" v-model="coin">
+                <option value="Bitcoin">Bitcoin</option>
+                <option value="Ethereum">Ethereum</option>
+                <option value="EOS">EOS</option>
+              </select>
+              <input 
+                type="text" 
+                class="form-control form-control-sm col-md-6 offset-md-1" 
+                id="coin-alias" 
+                placeholder="Alias"
+                v-model="alias">
             </div>
           </div>
           <div class="form-group">
             <div class="row">
-              <label class="col-md-2 control-label col-form-label-sm" for="coin-address">Address</label>
-              <div class="col-md-10">
-                <input type="text" class="form-control form-control-sm" id="coin-address" placeholder="Address"
-                  disabled>
+              <div class="form-check col-md-6">
+                <label class="form-check-label">
+                  <input 
+                    class="form-check-input" 
+                    type="radio" 
+                    id="address" 
+                    value="Address" 
+                    v-model="walletType"
+                    checked>
+                  Blockchain address
+                </label>
               </div>
+              <div class="form-check col-md-6">
+                <label class="form-check-label">
+                  <input 
+                    class="form-check-input" 
+                    type="radio" 
+                    id="ballance" 
+                    value="Ballance"
+                    v-model="walletType">
+                  Local wallet
+                </label>
+              </div>
+            </div>
+          </div>
+          <div 
+            class="form-group"
+            v-show="walletType === 'Address'">
+            <div class="row">
+              <input 
+                type="text" 
+                class="form-control form-control-sm col-md-12" 
+                id="coin-address"
+                v-model="address" 
+                placeholder="Address">
+            </div>
+          </div>
+          <div 
+            class="form-group"
+            v-show="walletType === 'Ballance'">
+            <div class="row">
+                <input 
+                  type="number" 
+                  class="form-control form-control-sm col-md-12" 
+                  id="coin-balance" 
+                  v-model="ballance"
+                  placeholder="Ballance">
             </div>
           </div>
           <div class="form-group">
             <div class="row">
-              <div class="form-check col-md-4">
+              <div class="form-check col">
                 <label class="form-check-label col-form-label-sm">
-                  <input class="form-check-input form-control-sm" type="checkbox" name="localWallet"> Local wallet
+                  <input 
+                    class="form-check-input form-control-sm" 
+                    type="radio" 
+                    value="None"
+                    v-model="group"> 
+                    No group
                 </label>
               </div>
-              <label class="col-md-2 offset-md-1 control-label col-form-label-sm" for="coin-balance">Balance</label>
-              <div class="col-md-5">
-                <input type="number" class="form-control form-control-sm" id="coin-balance" placeholder="Ballance"
-                  disabled>
+              <div class="form-check col">
+                <label class="form-check-label col-form-label-sm">
+                  <input 
+                    class="form-check-input form-control-sm" 
+                    type="radio" 
+                    value="Same"
+                    v-model="group"> 
+                    Group with same coin
+                </label>
               </div>
             </div>
-          </div>
-          <div class="form-group">
             <div class="row">
-              <div class="form-check col-md-7">
+              <div class="form-check col-5">
                 <label class="form-check-label col-form-label-sm">
-                  <input class="form-check-input form-control-sm" type="radio" name="group" value="nope" checked> No group
-                </label>
-                <label class="form-check-label col-form-label-sm">
-                  <input class="form-check-input form-control-sm" type="radio" name="group" value="same"> Group with same coin
-                </label>
-                <label class="form-check-label col-form-label-sm">
-                  <input class="form-check-input form-control-sm" type="radio" name="group" value="another"> Add to another group
+                  <input 
+                    class="form-check-input form-control-sm" 
+                    type="radio" 
+                    value="Another"
+                    v-model="group"> 
+                    Add to another group
                 </label>
               </div>
-              <div class="col-md-5">
-                <input class="form-control form-control-sm" type="text" name="group-name" disabled>
+              <div class="col-7 right-zero">
+                <input 
+                  class="form-control form-control-sm" 
+                  type="text" 
+                  name="group-name"
+                  v-model="groupName">
               </div>
             </div>
           </div>
@@ -62,11 +117,15 @@
         <div class="col-md-2">
           <div class="form-check">
             <label class="form-check-label col-form-label-sm">
-              <input class="form-check-input form-control-sm" type="checkbox" name="power" checked> Active
+              <input 
+                class="form-check-input form-control-sm" 
+                type="checkbox" 
+                name="power" 
+                v-model="isActive"> 
+                Active
             </label>
           </div>
-          <button class="btn btn-warning btn-block btn-sm">Edit</button>
-          <button class="btn btn-danger btn-block btn-sm">Delete</button>
+          <button class="btn btn-warning btn-block btn-sm">Save</button>
         </div>
       </div>
     </form>
@@ -75,6 +134,18 @@
 
 <script>
 export default {
+  data () {
+    return {
+      coin: '',
+      alias: '',
+      walletType: 'Address',
+      address: '',
+      ballance: '',
+      group: 'Same',
+      groupName: '',
+      isActive: true
+    }
+  },
   methods: {
   }
 }
@@ -83,6 +154,10 @@ export default {
 <style scoped>
 .form-horizontal {
   margin: 30px;
+}
+
+.right-zero {
+  padding-right: 0;
 }
 </style>
 
